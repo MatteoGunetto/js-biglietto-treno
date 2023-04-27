43d6ab34330dbefe724687f3d3fd878a97d8c0a6
MatteoGunetto <Matteogunetto@tiscali.it>
2023-04-27 15:05:18 +0200

    terminato

--- javascript/biglietto.js
+++ javascript/biglietto.js
@@ -3,3 +3,42 @@
 // va applicato uno sconto del 20% per i minorenni
 // va applicato uno sconto del 40% per gli over 65.
 // L’output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo).
+
+// variabili globali
+let distanzaKm, anniPasseggero, sconto, prezzo;
+
+
+distanzaKm = parseInt(prompt('Quanti km vuoi percorrere? '));
+
+anniPasseggero = parseInt(prompt('Quanti anni hai? '));
+
+    // il prezzo del biglietto è definito in base ai km (0.21 € al km)
+prezzo = distanzaKm * 0.21 ;
+
+    // va applicato uno sconto del 20% per i minorenni
+
+if( anniPasseggero < 18){
+
+    sconto = (prezzo * 20) /100;
+    prezzo = prezzo - sconto; 
+
+    // va applicato uno sconto del 40% per gli over 65.
+
+}else if( anniPasseggero >  65)
+{
+    sconto = (prezzo * 40) /100;
+    prezzo = prezzo - sconto; 
+   
+
+}
+
+else{
+    sconto = 0;
+    
+}
+console.log(sconto)
+console.log(prezzo)
+
+// L’output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo).
+
+document.getElementById('totale').innerHTML = prezzo.toFixed(2); 
